a84df43e1de04ab404b1b9e190bd14201ae00af9
Alex <alexthegeek2001@gmail.com>
2021-01-18 15:26:23 +0530

    logout

--- createAccount.php
+++ createAccount.php
@@ -17,14 +17,17 @@
         </style>
     </head>
     <body>
-
+    
         <div id="login" class="offset">
+        
             <div class="col-12 narrow text-center">
+            
                 <h3>Choose an account to create</h3>
                 <hr class="clients-hr">
                 <a class="btn btn-secondary btn-md" href="keellsRegistration.php">Register for keells</a> <br>
                 <a class="btn btn-secondary btn-md" href="doaRegistration.php">Register for Agriculture Department</a>
             </div> 
+            <a class="btn btn-secondary btn-md" href="logout.php">Log out</a>
         </div>
         <?php
             include('footer.php');
